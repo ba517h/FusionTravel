7e461a4d988c9b2783a8b1467aeb115fa2cc1fa6
Mohammed Basith <mhdbazi@gmail.com>
2017-01-29 00:55:25 +0530

    Final Commit
    Added Latest news from blog section.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,28 @@ var App = angular.module('App', []);
 App.controller('listCtrl', function($scope, $http) {
   $http.get('country.json')
        .then(function(res){
-          $scope.contents = res.data;                
+       	var vm=$scope;
+         vm.contents = res.data;
+
+          vm.activeCountry = {}; 
+ 
+    // Adding the Search property to be used in the ng-model
+    		vm.search = "";
+ 
+ 
+ 
+    vm.changeActiveCountry = changeActiveCountry;
+ 
+    function changeActiveCountry(index){
+        vm.activeCountry = index;
+    }                
+        });
+});
+App.controller('listCtrl1', function($scope, $http) {
+  $http.get('blog.json')
+       .then(function(res){
+       	var vm=$scope;
+         vm.contents1 = res.data;
+              
         });
 });
